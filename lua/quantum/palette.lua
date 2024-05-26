002e3daf0a184a5edbb0268bef42fb7f7142f4d5
Walter Jenkins <walter@transit.chat>
2024-05-26 09:35:42 -0500

    Update palette.lua

--- lua/quantum/palette.lua
+++ lua/quantum/palette.lua
@@ -1,3 +1,55 @@
+
+-- DEFAULT COLORS
+local dark_palette = {
+  fg = "#d7d7db",
+  fg_sec = "#c3c3c6",
+  fg_alt = "#939395",
+  fg_gutter = "#4a4a4f",
+
+  bg = "#2a2a2e",
+  bg_sec = "#303034",
+  bg_alt = "#38383d",
+  bg_dark = "#232327",
+
+  border = "#1b1b1d",
+  bracket_match = "#58585C",
+  guides = "#414145",
+  selection = "#393f4c",
+  fold = "#2F323A",
+  comment = "#737373",
+
+  white = "#cecece",
+  blue = "#6b89ff",
+  cyan = "#75bfff",
+  green = "#86de74",
+  magenta = "#ff8de9",
+  orange = "#fc9867",
+  purple = "#b98eff",
+
+  ui_cyan = "#48acf2",
+  ui_green = "#6cca69",
+  ui_red = "#f25e72",
+  ui_yellow = "#ffd866",
+  ui_purple = "#b058dd",
+
+  bg_white = "#080A08",
+  bg_cyan = "#2a3e50",
+  bg_green = "#324434",
+  bg_purple = "#3f2e4b",
+  bg_red = "#4c2f36",
+  bg_yellow = "#4F4734",
+
+  git_yellow = "#e0b17f",
+  git_green = "#29aa84",
+  git_red = "#e6668b",
+
+  diff_yellow = "#363130",
+  diff_green = "#243130",
+  diff_red = "#362930",
+  diff_text = "#4f443b",
+}
+
+
 local light_palette = {
   fg = "#38383d",
   fg_sec = "#4a4a4f",
@@ -96,54 +148,6 @@ local night_palette = {
   diff_text = "#4f443b",
 }
 
-local dark_palette = {
-  fg = "#d7d7db",
-  fg_sec = "#c3c3c6",
-  fg_alt = "#939395",
-  fg_gutter = "#4a4a4f",
-
-  bg = "#2a2a2e",
-  bg_sec = "#303034",
-  bg_alt = "#38383d",
-  bg_dark = "#232327",
-
-  border = "#1b1b1d",
-  bracket_match = "#58585C",
-  guides = "#414145",
-  selection = "#393f4c",
-  fold = "#2F323A",
-  comment = "#737373",
-
-  white = "#cecece",
-  blue = "#6b89ff",
-  cyan = "#75bfff",
-  green = "#86de74",
-  magenta = "#ff8de9",
-  orange = "#fc9867",
-  purple = "#b98eff",
-
-  ui_cyan = "#48acf2",
-  ui_green = "#6cca69",
-  ui_red = "#f25e72",
-  ui_yellow = "#ffd866",
-  ui_purple = "#b058dd",
-
-  bg_white = "#080A08",
-  bg_cyan = "#2a3e50",
-  bg_green = "#324434",
-  bg_purple = "#3f2e4b",
-  bg_red = "#4c2f36",
-  bg_yellow = "#4F4734",
-
-  git_yellow = "#e0b17f",
-  git_green = "#29aa84",
-  git_red = "#e6668b",
-
-  diff_yellow = "#363130",
-  diff_green = "#243130",
-  diff_red = "#362930",
-  diff_text = "#4f443b",
-}
 
 local M = {}
 
